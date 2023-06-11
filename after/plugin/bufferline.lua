c61081d8c1334c60cf94a0c371a1b11f847623ca
Sergio Teixeira <sergio@triggerise.org>
2023-06-11 11:04:13 +0100

    change theme and format lua files

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,11 +1,13 @@
 require("bufferline").setup({
     options = {
-      indicator = { style = "icon", icon = "▎" },
-      modified_icon = '●',
-      show_buffer_icons=true,
-      offsets = {{filetype = "NvimTree",
-         text = "File Explorer",
-     text_align = "center"}},
-      diagnostics = "nvim_lsp",
+        indicator = { style = "icon", icon = "▎" },
+        modified_icon = '●',
+        show_buffer_icons = true,
+        offsets = { {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            text_align = "center"
+        } },
+        diagnostics = "nvim_lsp",
     },
-  })
+})
--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,10 +1,10 @@
 local setup, null_ls = pcall(require, "null-ls")
 if not setup then
-	return
+    return
 end
 
 -- for conciseness
-local formatting = null_ls.builtins.formatting -- to setup formatters
+local formatting = null_ls.builtins.formatting   -- to setup formatters
 local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
 -- to setup format on save
@@ -14,31 +14,31 @@ local fmt = null_ls.builtins.formatting
 
 -- configure null_ls
 null_ls.setup({
-	-- setup formatters & linters
-	sources = {
-		formatting.markdownlint, -- markdown formatter
-		diagnostics.hadolint, -- docker formatter
-		diagnostics.flake8, -- Python
-		fmt.terraform_fmt, -- Terraform
+    -- setup formatters & linters
+    sources = {
+        formatting.markdownlint, -- markdown formatter
+        diagnostics.hadolint, -- docker formatter
+        diagnostics.flake8, -- Python
+        fmt.terraform_fmt, -- Terraform
         formatting.packer
-	},
-	-- configure format on save
-	on_attach = function(current_client, bufnr)
-		if current_client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({
-						filter = function(client)
-							--  only use null-ls for formatting instead of lsp server
-							return client.name == "null-ls"
-						end,
-						bufnr = bufnr,
-					})
-				end,
-			})
-		end
-	end,
+    },
+    -- configure format on save
+    on_attach = function(current_client, bufnr)
+        if current_client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.format({
+                        filter = function(client)
+                            --  only use null-ls for formatting instead of lsp server
+                            return client.name == "null-ls"
+                        end,
+                        bufnr = bufnr,
+                    })
+                end,
+            })
+        end
+    end,
 })
