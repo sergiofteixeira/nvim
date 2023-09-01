e8019bc5ff15ad1b2437246d58f252178529294a
Sergio Teixeira <sergio@triggerise.org>
2023-09-01 12:39:26 +0100

    chores: fix

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -38,10 +38,6 @@ require 'nvim-tree'.setup {
         --height = 30,
         hide_root_folder = false,
         side = 'left',
-        mappings = {
-            custom_only = false,
-            list = {}
-        },
         number = false,
         relativenumber = false,
         signcolumn = "yes"
