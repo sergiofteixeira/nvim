03203ac3c31004caa9aa9ec5b5d6a1dca93b4906
Sergio Teixeira <sergio@triggerise.org>
2024-05-14 12:14:04 +0100

    dettach yamlls if file is helm

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -16,12 +16,6 @@ local cfg = require("yaml-companion").setup {
             uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json"
         },
         {
-            name = "SealedSecret",
-            uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/bitnami.com/sealedsecret_v1alpha1.json"
-        },
-        -- schemas below are automatically loaded, but added
-        -- them here so that they show up in the statusline
-        {
             name = "Kustomization",
             uri = "https://json.schemastore.org/kustomization.json"
         },
@@ -191,6 +185,34 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 lsp.setup()
+
+local function detach_yamlls()
+    local clients = vim.lsp.get_active_clients()
+    for client_id, client in pairs(clients) do
+        if client.name == "yamlls" then
+            vim.lsp.buf_detach_client(0, client_id)
+        end
+    end
+end
+
+local gotmpl_group = vim.api.nvim_create_augroup("_gotmpl", { clear = true })
+
+-- detach yamlls from helm files
+vim.api.nvim_create_autocmd("FileType", {
+    group = gotmpl_group,
+    pattern = "yaml",
+    callback = function()
+        vim.schedule(function()
+            local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+            for _, line in ipairs(lines) do
+                if string.match(line, "{{.+}}") then
+                    vim.defer_fn(detach_yamlls, 500)
+                    return
+                end
+            end
+        end)
+    end,
+})
 
 vim.diagnostic.config({
     virtual_text = true,
