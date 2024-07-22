720ab96eec2ade428b68be2599ebb8c09e901795
Sergio Teixeira <sergio@triggerise.org>
2024-07-22 12:18:01 +0100

    update lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,37 +1,31 @@
-local function get_schema()
-    local schema = require("yaml-companion").get_buf_schema(0)
-    if schema.result[1].name == "none" then
-        return ""
-    end
-    return schema.result[1].name
-end
-
 require('lualine').setup {
-    --colorscheme = "vitesse",
+    colorscheme = "decay",
     options = {
         theme = 'auto',
     },
     sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff' },
-        lualine_c = { 'buffers' },
-        lualine_x = { 'fileformat', 'filetype', get_schema },
-        lualine_y = { 'progress' },
-        lualine_z = {
+        lualine_b = { 'branch' },
+        lualine_c = { {
+            'filename',
+            file_status = true,
+            path = 1
+        } },
+        lualine_d = { 'progress' },
+        lualine_e = {
             { 'diagnostics',
                 sources = { 'nvim_diagnostic', 'nvim_lsp' },
                 sections = { 'error', 'warn', 'info', 'hint' },
                 diagnostics_color = {
-                    -- Same values as the general color option can be used here.
-                    error = 'DiagnosticError', -- Changes diagnostics' error color.
-                    warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
-                    info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
-                    hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
+                    error = 'DiagnosticError',
+                    warn  = 'DiagnosticWarn',
+                    info  = 'DiagnosticInfo',
+                    hint  = 'DiagnosticHint',
                 },
                 symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' },
-                colored = true,           -- Displays diagnostics status in color if set to true.
-                update_in_insert = false, -- Update diagnostics in insert mode.
-                always_visible = false,   -- Show diagnostics even if there are none.
+                colored = true,
+                update_in_insert = true,
+                always_visible = false,
             }
         }
     }
