df0ec469a8ad91017fd7e667c90dd4b44123735d
Sergio Teixeira <sergio@triggerise.org>
2024-09-13 15:53:07 +0100

    adjust settings gruvbox

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,6 +1,17 @@
 require("gruvbox").setup({
     --contrast = "soft",
     contrast = "",
+    terminal_colors = false,
+    undercurl = false,
+    underline = false,
+    bold = false,
+    italic = {
+        strings = false,
+        emphasis = false,
+        comments = true,
+        operators = false,
+        folds = false,
+    },
 })
 vim.cmd("set background=dark")
 vim.cmd("colorscheme gruvbox")
