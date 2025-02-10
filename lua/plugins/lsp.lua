2fc09df3cd4c875b472a2e4dc524e63914107230
Sergio Teixeira <sergio@triggerise.org>
2025-02-10 14:05:05 +0000

    ignore helm
    Signed-off-by: Sergio Teixeira <sergio@triggerise.org>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -236,6 +236,33 @@ return {
         },
       }),
     })
+    local function detach_yamlls()
+      local clients = vim.lsp.get_active_clients()
+      for client_id, client in pairs(clients) do
+        if client.name == "yamlls" then
+          vim.lsp.buf_detach_client(0, client_id)
+        end
+      end
+    end
+
+    local gotmpl_group = vim.api.nvim_create_augroup("_gotmpl", { clear = true })
+
+    -- detach yamlls from helm files
+    vim.api.nvim_create_autocmd("FileType", {
+      group = gotmpl_group,
+      pattern = "yaml",
+      callback = function()
+        vim.schedule(function()
+          local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+          for _, line in ipairs(lines) do
+            if string.match(line, "{{.+}}") then
+              vim.defer_fn(detach_yamlls, 500)
+              return
+            end
+          end
+        end)
+      end,
+    })
 
     cmp.event:on(
       'confirm_done',
