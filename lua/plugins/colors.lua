655b0f5a49f86a0b5badc0e15e563f1470340e52
Sergio Teixeira <sergiofpteixeira@gmail.com>
2026-01-22 12:14:06 +0000

    update

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,7 +1,18 @@
-return {
-  'jacoborus/tender.vim',
-  name = tender,
+return
+{
+  'olivercederborg/poimandres.nvim',
+  lazy = false,
+  priority = 1000,
   config = function()
-    vim.cmd "colorscheme tender"
+    require('poimandres').setup {
+      -- leave this setup function empty for default config
+      -- or refer to the configuration section
+      -- for configuration options
+    }
+  end,
+
+  -- optionally set the colorscheme within lazy config
+  init = function()
+    vim.cmd("colorscheme poimandres")
   end
 }
