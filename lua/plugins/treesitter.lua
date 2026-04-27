407eebf9300effde77b67ae7ccdd04080740d459
Sergio Teixeira <sergiofpteixeira@gmail.com>
2026-04-27 10:09:57 +0100

    update to 0.12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
+  branch = "main",
+  main = "nvim-treesitter",
+  config = function()
+    local parsers = {
       "javascript",
       "typescript",
       "lua",
@@ -9,12 +11,24 @@ return {
       "go",
       "terraform",
       "hcl",
-    },
-    highlight = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-    },
-  },
+    }
+
+    local installed = require("nvim-treesitter.config").get_installed()
+    local missing = vim.iter(parsers)
+      :filter(function(parser)
+        return not vim.tbl_contains(installed, parser)
+      end)
+      :totable()
+
+    if #missing > 0 then
+      require("nvim-treesitter").install(missing)
+    end
+
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(ev)
+        pcall(vim.treesitter.start, ev.buf)
+        vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
 }
